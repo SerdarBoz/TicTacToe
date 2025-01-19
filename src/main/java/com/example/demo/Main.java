eaf023f1040fafc4001ab723ac1fe95b34a79b6e
Serdar Bozkurt <r0960532@uccl.be>
2025-01-19 14:41:54 +0100

    Aanpassing in de main

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        //test
         try {
             Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
             primaryStage.setScene(new Scene(root));
