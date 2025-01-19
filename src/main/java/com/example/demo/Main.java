79fbc09fef65dc6a4ef47230332e0dfe0a6720fc
Serdar Bozkurt <r0960532@uccl.be>
2025-01-19 14:40:39 +0100

    Aanpassing in de main

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -6,14 +6,22 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+            primaryStage.setScene(new Scene(root));
+            primaryStage.setTitle("Tic-Tac-Toe: Best of Five");
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Fout bij het laden van sample.fxml: " + e.getMessage());
+        }
+
     }
 
     public static void main(String[] args) {
